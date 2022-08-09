1a454542066b1b958d0020056b7187f8c3c6733a
Denis Sidorenko <70846761+DenisSidoren@users.noreply.github.com>
2022-08-09 14:38:56 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'FEDOT'
+project = 'test FEDOT'
 copyright = '2020-2022, NSS Lab'
 author = 'NSS Lab'
 
